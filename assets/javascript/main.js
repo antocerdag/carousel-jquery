15487a9d59458e88282c84b9543199ad257cf79c
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-06 18:09:45 -0400

    agregando estilo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,13 @@
-main.js
+$(document).ready(function() {
+	$("#flechaizq").click(function(){
+		
+		$("#cajacarousel ul").prepend($("#cajacarousel ul li:last"));
+	});
+
+	$("#flechader").click(function(){
+
+		$("#cajacarousel ul").append($("#cajacarousel ul li:first"));
+	})
+
+	
+});
