68a61e6a33d9b84646f59b7c827d568922d44e77
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-06 18:28:37 -0400

    agregando desplazamiento 5seg

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+	//Desplazamiento automatico cada 5seg
+	setInterval("$('#flechaizq').click()",5000);
+	setInterval("$('#flechader').click()",5000);
 	$("#flechaizq").click(function(){
 		
 		$("#cajacarousel ul").prepend($("#cajacarousel ul li:last"));
